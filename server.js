6d44089ad4a50855cb7e089f040fc50766549109
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-08-13 14:19:36 -0400

    added front end to back end

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 const { animals } = require("./data/animals.json");
 
 function filterByQuery(query, animalsArray) {
@@ -112,6 +113,18 @@ app.post("/api/animals", (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/.zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.listen(PORT, () => {
